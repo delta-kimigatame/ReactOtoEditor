b8bdf2b44ad32e1452689c60327fa48f5c74c529
delta_kimigatame <sankaku.kuro@gmail.com>
2024-06-27 20:17:32 +0900

    フォルダを切り替えた際に落ちる不具合の修正

--- src/Top/TargetDirDialog/TargetDirDialogCheckList.tsx
+++ src/Top/TargetDirDialog/TargetDirDialogCheckList.tsx
@@ -12,20 +12,22 @@ import { Typography } from "@mui/material";
  * @param props {@link TargetDirDialogCheckListProps}
  * @returns 原音設定対象ディレクトリを選択するためのダイアログのボディ内のoto.ini文字化けを確認するエリア
  */
-export const TargetDirDialogCheckList: React.FC<TargetDirDialogCheckListProps> = (props) => {
+export const TargetDirDialogCheckList: React.FC<
+  TargetDirDialogCheckListProps
+> = (props) => {
   const { t } = useTranslation();
-
   return (
     <>
       <Box>
         <Typography variant="h6">{t("targetDirDialog.encodeCheck")}</Typography>
         <Divider />
-        {props.oto.GetLines()[props.targetDir].map((l) => (
-          <>
-            <Typography variant="caption">{l}</Typography>
-            <Divider />
-          </>
-        ))}
+        {props.oto.GetLines()[props.targetDir] !== undefined &&
+          props.oto.GetLines()[props.targetDir].map((l) => (
+            <>
+              <Typography variant="caption">{l}</Typography>
+              <Divider />
+            </>
+          ))}
       </Box>
     </>
   );
@@ -36,4 +38,4 @@ export interface TargetDirDialogCheckListProps {
   targetDir: string | null;
   /** 仮に読み込んだoto.iniのデータ */
   oto: Oto;
-};
+}
