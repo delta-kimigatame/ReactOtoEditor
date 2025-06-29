9331aab0f523f1ce46822480943b564e1902473c
delta_kimigatame <sankaku.kuro@gmail.com>
2025-06-29 16:40:11 +0900

    GetLinearGradientの分離とtesting

--- src/components/Header/HeaderMenuItem/ColorAvatar.tsx
+++ src/components/Header/HeaderMenuItem/ColorAvatar.tsx
@@ -3,6 +3,7 @@ import { PaletteMode } from "@mui/material";
 
 import Avatar from "@mui/material/Avatar";
 import { specColor } from "../../../config/colors";
+import { GetLinearGradient } from "../../../utils/GetLinearGradient";
 
 /**
  * 色設定メニューにサンプルで表示するアイコン
@@ -26,18 +27,4 @@ export const ColorAvatar: React.FC<{
       {" "}
     </Avatar>
   );
-};
-/**
- * 色設定を受け取り、グラデーション設定用の文字列を返す。
- * @param mode ダークモードかライトモードか
- * @param color キャンバスの色設定
- * @returns グラデーション設定用の文字列
- */
-export const GetLinearGradient = (mode: PaletteMode, color: string): string => {
-  let value = "linear-gradient(to top";
-  specColor[color][mode].forEach((c) => {
-    value += ",rgb(" + c.r + "," + c.g + "," + c.b + ")";
-  });
-  value += ")";
-  return value;
 };
